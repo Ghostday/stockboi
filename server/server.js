32d1dc08f317086dddbb8f563ecca84fecb08fad
Ghostday <fablekey@gmail.com>
2021-12-03 02:23:48 +1100

    MongoDB Connection is Secure and Setup with ENV secrets functioning properly

--- server/server.js
+++ server/server.js
@@ -1,19 +1,31 @@
+if (process.env.NODE_ENV !== 'production') {
+  require("dotenv").config();
+}
+
+const path = require("path");
 const express = require("express");
 const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
-app.use(cors());
-app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
- 
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server is running on port: ${port}`);
-});
+
+const { MongoClient } = require("mongodb");
+
+(async () => {
+  const mongo = new MongoClient(process.env.MONGODB, {auth:({username: process.env.MONGOUSERNAME, password: process.env.MONGOPASSWORD})});
+
+  try {
+    await mongo.connect();
+  } catch (err) {
+    console.log('Failed to connect to MongoDB');
+    console.log(err);
+    process.exit(1);
+  }
+
+  console.log('Connected to MongoDB');
+
+  app.use(express.static(path.join(__dirname, '../dist')));
+  app.listen(process.env.PORT);
+
+  console.log(`HTTP Listening on ${process.env.PORT}`);
+
+  const insert = await mongo.db('stocks').collection("documents").insertMany([{a: 1}, {b: 2}, {c: 3}])
+  console.log('Inserted documents', insert)
+})();
