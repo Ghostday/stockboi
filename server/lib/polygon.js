00122afd3d2f7cdd60340e8f59c72a8d8069ab4e
Ghostday <fablekey@gmail.com>
2021-12-07 10:21:49 +1100

    Started building custom Polygon API Library

--- server/lib/polygon.js
+++ server/lib/polygon.js
@@ -1,7 +1,23 @@
 const APIKey = process.env.POLYGONKEY;
 const bUrl = "https://api.polygon.io"
 
-
-export function aggregates(ticker, multiplier, timespan, from, to, limit) {
-    return `${bUrl}/v2/aggs/ticker/${ticker}/range/1/day/2021-07-22/2021-07-22?adjusted=true&sort=asc&limit=120&apiKey=g2xdpDPCcfjpzTny8bEKKVpseRkmWNBd`
+function getFormattedDate(date) {
+    const year = date.getFullYear();
+  
+    const month = (1 + date.getMonth()).toString();
+    month = month.length > 1 ? month : '0' + month;
+  
+    const day = date.getDate().toString();
+    day = day.length > 1 ? day : '0' + day;
+    
+    return month + '-' + day + '-' + year;
 }
+
+
+function dailyAggregate(ticker) {
+    const date = getFormattedDate(new Date)
+    console.log(date)
+    return `${bUrl}/v2/aggs/ticker/${ticker}/range/1/day/${date}/${date}?adjusted=true&sort=asc&limit=120&apiKey=g2xdpDPCcfjpzTny8bEKKVpseRkmWNBd`
+}
+
+console.log(dailyAggregate('appl'))
