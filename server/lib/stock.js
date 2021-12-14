82d4a638593faddc9a114d95578590ea267beb5a
Chris Gemmell <fablekey@gmail.com>
2021-12-14 21:28:38 +1100

    Added stock methods

--- server/lib/stock.js
+++ server/lib/stock.js
@@ -2,7 +2,7 @@ class Stock {
     constructor(ticker, price, amount) {
         this.ticker = ticker;
         this.price = price;
-        this.#amount = amount;
+        this.amount = amount;
     }
 
     set price(price) {
@@ -11,3 +11,20 @@ class Stock {
 
     
 }
+
+import StockFetch from '../exampleResponse.json'
+
+const stockData = StockFetch.tickers
+console.log(stockData.length)
+let stocksInUse = stockData.filter((ticker) => {
+    return ticker.todaysChange != 0
+})
+stocksInUse //
+
+console.log(stocksInUse.length)
+
+let stocksUptrending = stocksInUse.filter((ticker) => {
+    return ticker.todaysChange > 0
+})
+
+stocksUptrending.length
