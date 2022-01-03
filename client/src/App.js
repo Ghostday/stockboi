31fcf898ce0cc764b717494783b9515dff956f99
Chris Gemmell <fablekey@gmail.com>
2022-01-04 10:41:07 +1100

    More modifications to the front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from 'react'
-import Stock from './Components/Stock'
+import StockList from './Components/StockList'
 
 function App() {
-  const [gainers, setGainers] = useState(false)
   
-  useEffect(() => {
-    fetch(`https://api.polygon.io/v2/snapshot/locale/us/markets/stocks/gainers?apiKey=g2xdpDPCcfjpzTny8bEKKVpseRkmWNBd`)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      setGainers(data.tickers)
-    })
-  })
 
   return (
     <div className="App">
     <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        { gainers ?
-          gainers.map((stock, key) => {
-            return <Stock stock={stock.ticker}/>
-          }) : <h1>Not Loaded</h1>
-        }
+
       </header>
+      <StockList/>
     </div>
   );
 }
